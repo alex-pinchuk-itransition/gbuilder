1731f2829cca212db407cfd794a179b98f9c9abc
Alex Sander <mne@tut.by>
2016-03-09 10:24:12 +0300

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+// import FriendListApp from './FriendListApp';
+import * as reducers from '../reducers';
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<Provider store={store}>
+					// {() => <FriendListApp /> }
+					Hello World!
+				</Provider>
+
+			</div>
+		);
+	}
+}
+
